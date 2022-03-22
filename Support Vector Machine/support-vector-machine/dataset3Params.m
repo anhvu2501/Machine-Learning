ee37d864b786c9a7462bda895703a761ba51a47e
anhvu2501 <vu25ck28@gmail.com>
2022-03-22 16:10:08 +0700

    SVM with Gaussian Kernel

--- Support Vector Machine/support-vector-machine/dataset3Params.m
+++ Support Vector Machine/support-vector-machine/dataset3Params.m
@@ -23,11 +23,28 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+results = zeros(64,3); % result matrix to store 8^2 different results of 8^2 different models
+error_row = 0; % mark the current error row when running loops (just like an index)
+
+C_test = [0.01 0.03 0.1 0.3 1 3 10 30];
+sigma_test = [0.01 0.03 0.1 0.3 1 3 10 30];
+for i = 1 : size(C_test, 2),
+   for j = 1 : size(sigma_test, 2),
+     error_row = error_row + 1; % increase by 1 for each loop
+     model = svmTrain(X, y, C_test(i), @(x1, x2) gaussianKernel(x1, x2, sigma_test(j))); % train SVM model by using Gaussian Kernel
+     predictions = svmPredict(model, Xval); % The vector contains all the predictions from the SVM (predict the label on the cross validation set)
+     prediction_error = mean(double(predictions ~= yval)); % for evaluating the error on the cross validation set 
+     % For classification, the error is defined as the fraction of the cross validation examples that were classified incorrectly
+     results(error_row, :) = [C_test(i), sigma_test(j), prediction_error]; % store each C_test, sigma_test and the prediction_error correspondingly
+   end
+end
 
 
+sorted_results = sortrows(results, 3); % sort matrix by column 3 ascendingly (sort by prediction_error)
 
-
-
+% Then we can have the values for C and sigma
+C = sorted_results(1, 1);
+sigma = sorted_results(1, 2);
 
 % =========================================================================
 
